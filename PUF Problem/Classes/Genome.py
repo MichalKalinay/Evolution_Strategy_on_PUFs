e149530a0838716f5ab9b9f82388454d224fc513
MichalKalinay <michalkalinay@gmail.com>
2023-01-22 15:15:41 +0100

    Adjusted Genome type and generation

--- PUF Problem/Classes/Genome.py
+++ PUF Problem/Classes/Genome.py
@@ -1,18 +1,15 @@
 import random
-from typing import List, Tuple
+from typing import List
 
 
 class Genome:
     # List of the values of the delays between each gate representing the structure of a PUF
-    # Subject to be changed
-    values = List[Tuple[float]]
+    values = List[float]
 
     def __init__(self, length: int):
         self.values = []
         for i in range(0, length):
-            gate = (random.uniform(0, 1), random.uniform(0, 1))
-            self.values.append(gate)
+            self.values.append(random.uniform(0, 1))
 
     def __repr__(self):
         return f"{self.values}"
-
