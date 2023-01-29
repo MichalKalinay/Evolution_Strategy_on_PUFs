4940a4207977facea7ce637a6408c3ef8d5f8591
MichalKalinay <michalkalinay@gmail.com>
2023-01-29 16:04:44 +0100

    Adjustments for future fitness function

--- PUF Problem/Functions/selection.py
+++ PUF Problem/Functions/selection.py
@@ -1,19 +1,23 @@
 from random import choices
+from typing import Callable
 
-import fitness as fit
+from numpy import ndarray
+
+from Genome import Genome
 from Population import Population
 
-FitnessFunc = fit.fitness()
+FitnessFunc = Callable[[Genome, ndarray, ndarray], int]
 
 
 # Select the fittest 2 Genomes from a given Population to be the parents for the next generation
-def select_pair(population: Population, fitness_func: FitnessFunc) -> Population:
+def select_pair(population: Population, fitness_func: FitnessFunc,
+                challenges: ndarray, responses: ndarray) -> Population:
     result = Population(0, 0)
     # Solutions with a higher fitness value will be more likely to be chosen
     result.genomes.append(choices(
         population=population.genomes,
         # Weight of the choice is the fitness
-        weights=[fitness_func(genome) for genome in population.genomes],
+        weights=[fitness_func(genome, challenges, responses) for genome in population.genomes],
         # Select 2 parents
         k=2
     ))
